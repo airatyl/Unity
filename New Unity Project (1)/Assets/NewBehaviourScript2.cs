a12cc01072961056b6e113c67ff1951b3c2e2201
Student <student@mirea.ru>
2019-11-29 10:40:19 +0300

    lesson

---
+++ New Unity Project (1)/Assets/NewBehaviourScript2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript2 : MonoBehaviour
+{
+    [SerializeField] AudioSource audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    public void Plays()
+    {
+        audio.Play();
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+        }
+    }
+}
