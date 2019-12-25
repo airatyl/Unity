0acbac28448da5b8eedc65c4bfa7374606d8186d
project <Airatyla@mail.ru>
2019-12-25 18:01:01 +0300

    bg

---
+++ New Unity Project (1)/Assets/buttons.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttons : MonoBehaviour
+{
+    public GameObject begin;
+    public GameObject cube;
+    public GameObject rules;
+    public GameObject txt;
+    public void rule()
+    {
+        begin.SetActive(!begin.activeSelf);
+        txt.SetActive(!txt.activeSelf);
+    }
+    public void begins()
+    {
+        begin.SetActive(!begin.activeSelf);
+        rules.SetActive(!rules.activeSelf);
+        cube.SetActive(!cube.activeSelf);
+    }
+}
